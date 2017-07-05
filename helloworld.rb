5d00e357768b16b1ca78f7eb4f05d0dacb60026c
Gomasy <nyan@gomasy.jp>
2017-07-05 14:06:51 +0900

    Add helloworld.rb

---
+++ helloworld.rb
@@ -0,0 +1,20 @@
+ eval(%w(                [129902
+    2,676,               10,3039
+     314725             2,1152
+      671,17           347988
+       30,94         215099
+        71181       3100,1
+         66883     427851
+          0,151   7381,1
+           0,263 30644,
+            887,289623
+           84968  2543
+          0,138    01,33
+         91735      36615
+        87515        10].m
+       ap{|c          |print
+      "#{c.            to_s(3
+     6)}";p             rint"#
+   {32.ch                r}"}).join)
+   
+#=> ruby is a dynamic open source programming language with a focus on simplicity and productivity
